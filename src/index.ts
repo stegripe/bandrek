418bf937b61bb6ebbdb45dd04f6f48f982da9e52
mzrtamp <mozartrafi@clytage.org>
2024-02-28 23:39:26 +0700

    chore: better example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Sequelize } from "sequelize";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const client = new Sequelize({
     dialect: "mariadb",
-    host: "...",
-    password: "PaswordAmAn123###",
-    username: "TzyMantuv",
-    port: 6969
+    host: "localhost",
+    password: "youshallnotpass",
+    username: "root",
+    port: 3306,
 });
