a406c5f37403b814582fa5feada814f89134f4f8
Mednoob <ahmadyaqdhan123@gmail.com>
2024-03-07 17:03:16 +0900

    feat(api/tables): add db query

--- src/pages/api/tables.ts
+++ src/pages/api/tables.ts
@@ -5,6 +5,12 @@ export default async function tables(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const db = req.query["db"];
+    if (!db) {
+        res.status(400).json({ message: "Ngawur cik, mana nama dbnya???" });
+        return;
+    }
+
     const query = await connection.query("SHOW TABLES");
     res.status(200).json(query);
 }
