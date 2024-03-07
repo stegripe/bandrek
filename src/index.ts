0732915bdd3f863d53f175c36e0a297df4a04bfe
mzrtamp <mozartrafi@clytage.org>
2024-03-07 13:54:30 +0700

    chore: migrate credentials to dotenv file

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize";
 
 const client = new Sequelize({
     dialect: "mariadb",
-    port: 3306,
-    host: "localhost",
-    username: "root",
-    password: "youshallnotpass"
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "3306"),
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "youshallnotpass",
 });
 
 export default client;
