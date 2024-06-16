1e6440442ff6a02d60efe5bafe1bb82d92e90ea2
Syah Warid Ghani Akram <github@tira.my.id>
2024-06-16 20:14:51 +0700

    chore: change password & longer timeout

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ const client = new Sequelize({
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "3306"),
     username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "youshallnotpass",
+    password: process.env.DB_PASSWORD || "",
     dialectOptions: {
-        supportBigNumbers: true
-    }
+        supportBigNumbers: true,
+        connectTimeout: 60000000,
+        queryTimeout: 60000000,
+    },
 });
 
 export default client;
